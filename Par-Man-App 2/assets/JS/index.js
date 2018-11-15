e73e539b77a01c46661b47dca70ec739a9d1d555
dhmendoza <38670979+dhmendoza@users.noreply.github.com>
2018-11-14 16:21:58 -0800

    Add files via upload

---
+++ Par-Man-App 2/assets/JS/index.js
@@ -0,0 +1,3 @@
+alert(
+    "I am working now! We just had to clean up the file paths. From index.html the path for the JS file is assets/JS/index.js, and the path for css is assets>css>style.css"
+);
